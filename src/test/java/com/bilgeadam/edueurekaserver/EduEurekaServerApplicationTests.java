1cfb641aca3e9098989158f5d4bc6cc30c66c517
Samet <sametcoruk0604@gmail.com>
2022-01-29 12:29:26 +0300

    eureka client eklendi.

---
+++ src/test/java/com/bilgeadam/edueurekaserver/EduEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilgeadam.edueurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
